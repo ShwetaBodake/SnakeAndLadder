25fa07b649da6b9283e1875389dcec0b2da2f35d
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-24 14:38:37 +0530

    [Shweta] added Snake and Ladder program

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadder
+{
+class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-------------WelCome to Snake And Ladder Game-----------");
+        }
+    }
+}
+
